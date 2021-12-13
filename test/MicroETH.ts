7a461350119b34ceedfb266d21c9d8bdceb028b6
Andrew Davis <andy@191x.com>
2021-12-13 02:30:07 -0500

    Test work.

--- test/MicroETH.ts
+++ test/MicroETH.ts
@@ -3,6 +3,7 @@ import { ethers, waffle } from 'hardhat';
 import { expect } from 'chai';
 import { Signer } from 'ethers';
 import * as KSink from './util/KSink';
+import {waitWalletBalanceChange} from "./util/KSink";
 
 let contractName = "MicroETH";
 
@@ -22,9 +23,35 @@ describe(contractName, function () {
         await contract.deployed();
     });
 
-    it("Should verify that there is no initial supply.", async function () {
+    it("Should verify that there is no initial supply", async function () {
         let supply = (await contract.totalSupply()).toNumber();
         expect(supply).to.equal(0);
+    });
+
+    it("Should check that test wallet #1 contains no balance", async function () {
+        let balance = (await contract.balanceOf(walletAddress)).toNumber();
+        expect(balance).to.equal(0);
+    });
+
+    it("Should issue METH tokens after sending ether to deposit()", async function () {
+        let startBalance = (await wallet.getBalance());
+        let tx = (await contract.deposit({value: ethers.utils.parseEther("1.0")}));
+
+        let match = (await KSink.waitWalletBalanceChange(tx, wallet, startBalance, ethers.utils.parseEther("1.0")));
+        expect(match).to.be.true;
+    });
+
+    it("Should issue METH tokens after sending ether to fallback", async function () {
+        let startBalance = (await wallet.getBalance());
+
+        let txRequest = {
+            to: contract.address,
+            value: ethers.utils.parseEther("1.0")
+        };
+        let tx = wallet.sendTransaction(txRequest);
+
+        let match = (await KSink.waitWalletBalanceChange(tx, wallet, startBalance, ethers.utils.parseEther("1.0")));
+        expect(match).to.be.true;
     });
 
     // ...
--- test/util/KSink.ts
+++ test/util/KSink.ts
@@ -1,5 +1,23 @@
+import {ethers} from "hardhat";
+import {BigNumber, BigNumberish, Signer} from 'ethers';
 
 export const waitWriteMethod = async function(tx: Promise<any>): Promise<{ events: any }> {
     let txResponse = (await (await tx).wait());
     return { events: txResponse.events };
+}
+
+// Returns whether or not a wallet balance changes by (value) after a transaction has been mined.
+export const waitWalletBalanceChange = async function(tx: Promise<any>, wallet: Signer, startBalance: BigNumberish, value: BigNumberish): Promise<boolean> {
+    let txMined = (await (await tx).wait());
+    let newBalance = (await wallet.getBalance());
+
+    let cumulativeGasUsed = BigNumber.from(txMined.cumulativeGasUsed);
+    let effectiveGasPrice = BigNumber.from(txMined.effectiveGasPrice);
+    let gasUsed = cumulativeGasUsed.mul(effectiveGasPrice);
+
+    let temp = BigNumber.from(startBalance);
+    temp = temp.sub(gasUsed);
+    temp = temp.sub(BigNumber.from(value));
+
+    return temp.eq(newBalance);
 }
